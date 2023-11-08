3b80e8d534d76e90c26b2bbdaa63e8527b57045d
4nth0ny1 <acatullo4@gmail.com>
2023-11-07 22:32:28 -0600

    created aws lambda getUser function

---
+++ lambdas/getUser.js
@@ -0,0 +1,23 @@
+const responses = require("./API_Responses");
+
+exports.handler = async (event) => {
+  console.log("event", event);
+
+  if (!event.pathParameters || !event.pathParameters.ID) {
+    return responses._400({ message: "missing the ID from the path" });
+  }
+
+  let ID = event.pathParameters.ID;
+
+  if (data[ID]) {
+    return responses._200(data[ID]);
+  }
+
+  return responses._400({ message: "no ID in data" });
+};
+
+const data = {
+  1234: { name: "Anna Jones", age: 25, job: "journalist" },
+  5678: { name: "Chris Smith", age: 52, job: "engineer" },
+  9012: { name: "Tom Hague", age: 34, job: "lawyer" },
+};
